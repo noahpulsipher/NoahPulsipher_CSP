bc0b17cd6e94560f9ec7ac9b3f6117f85d877619
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-11-10 13:46:30 -0700

    Created a Global Impact screen that talks about Human Rights and why they are a necessity in our world and a app design to promote huan rights.

--- NoahPulsipher_CSP/Controller/GlobalImpactViewController.swift
+++ NoahPulsipher_CSP/Controller/GlobalImpactViewController.swift
@@ -8,20 +8,37 @@
 
 import UIKit
 
-class GlobalImpactViewController: UIViewController {
+class GlobalImpactViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
+        textMethod()
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    public func textMethod() -> Void
+    {
+        firstLabel.text = "Human rights is something that everyone on the planet should be garunteed, if without them, none of us would enjoy the lives we live."
+        secondLabel.text = "During my experiences I've had around the world, I saw people denied thier rights and suffer because of it."
+        imageView.image = #imageLiteral(resourceName: "Human App")
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBOutlet weak var imageView: UIImageView!
+   
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var secondLabel: UILabel!
+    
     /*
     // MARK: - Navigation
 
