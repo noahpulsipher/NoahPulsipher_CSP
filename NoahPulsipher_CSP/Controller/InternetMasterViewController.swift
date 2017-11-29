3971c4016147d5272e2e0e611dd04339bfb8e54a
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-11-29 11:52:50 -0700

    Building framework for the internet view controller.

---
+++ NoahPulsipher_CSP/Controller/InternetMasterViewController.swift
@@ -0,0 +1,79 @@
+//
+//  InternetViewController.swift
+//  NoahPulsipher_CSP
+//
+//  Created by Pulsipher, Noah on 10/26/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class InternetMasterViewController: UITableViewController
+    
+{
+    func (set) lazy var internetTopics : [String] =
+    {
+        return [
+        "Definitions",
+        "CSP",
+        "CTEC",
+        "Canyons",
+        "Twitter",
+        "Swift Guide"
+        ]
+    }()
+    
+    private var detailViewController : InternetDetailViewController?
+    
+    private func setup() -> Void
+    {
+        
+    }
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setup()
+        self.clearsSelectionOnViewWillAppear = false
+    }
+    
+    // MARK: - Table view data source
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return internetTopics.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewController
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel!.text = currentText
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
