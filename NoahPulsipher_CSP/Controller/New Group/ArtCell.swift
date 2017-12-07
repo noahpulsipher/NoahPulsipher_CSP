b934913563d08ae6bcdf1c40db58cc463dde216c
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-12-07 12:53:46 -0700

    Created artcell class.

---
+++ NoahPulsipher_CSP/Controller/New Group/ArtCell.swift
@@ -0,0 +1,18 @@
+//
+//  ArtCell.swift
+//  NoahPulsipher_CSP
+//
+//  Created by Pulsipher, Noah on 12/7/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ArtCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageName: UILabel!
+    
+}
