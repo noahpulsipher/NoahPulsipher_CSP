049747b5a45e2c09db6eb3bce2e0926c49d7cc7d
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-12-07 11:50:09 -0700

    Addig images into an array.

--- NoahPulsipher_CSP/Controller/CreativityViewController.swift
+++ NoahPulsipher_CSP/Controller/CreativityViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class CreativityViewController: UIViewController {
+class CreativityViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout
+{
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow : CGFloat = 3
+    
+    private lazy var artSelectio: [UIImage?] =
+    {
+        return [
+            UIImage(named: "cute"),
+            UIImage(named: "CSP Haiku"),
+            UIImage(named: "Swift Drawing"),
+            UIImage(named: "swift Sample"),
+            UIImage(named: "cute"),
+            UIImage(named: "cute"),
+            UIImage(named: "Swift Drawing"),
+            UIImage(named: "Swift Drawin")
+        ]
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
